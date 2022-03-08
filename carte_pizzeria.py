4403c0264444d6f8a45f09f4767779b3a81c7b35
Sebastien Morais <sebastien.morais@cea.fr>
2022-03-08 15:22:41 +0100

    MISC: Format To Pass Linter

--- carte_pizzeria.py
+++ carte_pizzeria.py
@@ -1,26 +1,43 @@
+"""Module Carte Pizzeria
+"""
 
 class CartePizzeriaException(Exception):
+    """Exception dedicated to CartePizzeria
+    """
     pass
 
+
 class CartePizzeria:
+    """Carte Pizzeria Class
+    """
 
     def __init__(self):
         self.__pizzas = []
 
     @property
     def pizzas(self):
+        """Returns pizzas.
+        """
         return self.__pizzas
 
     def is_empty(self):
+        """Return True if the CartePizzeria is empty, False otherwise.
+        """
         return len(self.pizzas) == 0
 
     def nb_pizzas(self):
+        """Return the number of pizzas
+        """
         return len(self.pizzas)
 
     def add_pizza(self, pizza):
+        """Add a new pizza
+        """
         self.pizzas.append(pizza)
 
     def remove_pizza(self, name):
+        """Remove a pizza
+        """
         found = False
         for pos, inner_pizza in enumerate(self.pizzas):
             if inner_pizza.name == name:
